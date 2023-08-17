fbdd1a1cfc28908fe6c6725bb7d1f7716da6905e
Omar Abou-Harb <60022342+sudo-omar@users.noreply.github.com>
2023-08-16 21:36:16 -0700

    add dimension slider

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ for(i = 0; i < 16; i++) {
   }
 }
 
+boardDimension = 500.0;
+
 //color picker
 let color = "black";
 const colorInput = document.querySelector('#color');
@@ -24,6 +26,35 @@ eraserButton.addEventListener('click', colorToWhite);
 const clearButton = document.querySelector("#clearAll");
 clearButton.addEventListener('click', clear);
 
+const range = document.querySelector("#rangeInput");
+range.addEventListener('input', dimensionChange);
+
+const rangeValue = document.querySelector('#rangeValue');
+
+function dimensionChange() {
+  //sets range text to value of input element
+  rangeValue.textContent = this.value;
+  //deletes prior board
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  //remake board
+  for(i = 0; i < this.value; i++) {
+    row = document.createElement("div");
+    container.appendChild(row);
+    row.classList.add("row");
+    for(j = 0; j < this.value; j++) {
+      column = document.createElement("div");
+      row.appendChild(column);
+      column.classList.add("column");
+      column.style.width = parseFloat(boardDimension / this.value) + "px";
+      column.style.height = parseFloat(boardDimension / this.value) + "px";
+      column.addEventListener('mouseenter', doThis);
+      column.addEventListener('mousedown', doThis)
+    }
+  }
+
+}
 function clear() {
   allBoxes = document.querySelectorAll(".column");
   allBoxes.forEach(column => column.style.backgroundColor = "white");
@@ -40,6 +71,6 @@ function changeColor() {
 function doThis(event) {
 	if(event.buttons == 1) {
 		this.style.backgroundColor = color;
-    console.log(getComputedStyle(this).getPropertyValue("opacity"));
+    
 	}
 }
